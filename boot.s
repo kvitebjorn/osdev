2656ae1a666745f2fb0ce8af00aadf64adcff9fa
kvitebjorn <harintonkairu@gmail.com>
2024-08-20 20:32:56 -0600

    spit out some more machine info and implement primitive stack

--- boot.s
+++ boot.s
@@ -2,9 +2,11 @@
 
 .section .data
 
-welcome: .ascii "Welcome to Noomi's World!\n\0"
+welcome: .ascii "Welcome to Noomi's World!\n\0" # Noomi my dog's name lol
 iamhart: .ascii "I am HART \0"
 newline: .ascii "\n\0"
+machinemode: .ascii "Mode: Machine\0"
+interrupts_status: .ascii "Interrupts: "
 
 _hartlock:
 	.skip 1,0
@@ -12,31 +14,77 @@ _hartlock:
 _scratchpad:
 	.skip 1024,0
 
+_stacks:
+	.skip 8192,0 # 8 cores supported, this space is used no matter what
+
 .section .text.init
 .global _start
 
 _start:
 	csrr t0, mhartid # get info about hardware threads (aka cores) 
+
+	la t1, _stacks # set up stack for each hart
+	li t2, 1024 # each hart gets 1024 bytes
+	mul t3, t0, t2 # calculate this hart's starting point in the stack 
+	add sp, t3, t1 # initialize stack pointer to starting point for hart
+
+	csrr t0, mhartid
 	bnez t0, _announce # wait until we are on the 0 thread 
 
 	call _setup_uart # configure UART in order to write out to the world
-
 	la a0, welcome # prep our arg register for the welcome message
 	call _write_uart # write the welcome message 
 
+	call _sanity_check # check machine status mode
 	j _announce # announce our HART ID
 
 	wfi 
 
+.macro push
+	sd ra, 0(sp) # push our ra onto the stack
+	add sp, sp, 8 # increment the stack pointer
+.endm
+
+.macro pop
+	add sp, sp, -8 # decrement the stack pointer
+	ld ra, 0(sp) # pop the stack pointer into our return address
+.endm
+
+_sanity_check:
+	push
+	csrr t0, mstatus # read the machine status register
+	li t1, 0xC00 # MPP is bits 11-12, so we need this mask for AND'ing 
+	and t0, t1, t2 # it will be non-zero if we are in machine mode
+	beq t1, t2, _sanity_check_end # if we aren't in machine mode, end
+	la a0, machinemode # otherwise point to our machine mode string
+	call _writeln # write the machine mode string	
+
+_sanity_check_interrupts:
+	csrr t0, mstatus
+	la a0, interrupts_status 
+	call _write_uart
+
+	li t1, 0x8 # MIE is bit 4, so we need this mask for AND'ing 
+	and t0, t1, t2 # isolate the bits we want
+	li t3, 0x30 # '0'
+	srai t2, t2, 3 # shift to get the actual number
+	add t1, t2, t3 # calculate our ASCII number, offset from '0'
+	la a0, _scratchpad # point to our scratchpad
+	sb t1, 0(a0) # store this char in our scratchpad
+	call _writeln # write it out
+
+_sanity_check_end:
+	pop
+	ret
+
 # a0 should contain the address of string to print
-# writeln will write this string to the serial UART device,
-#   followed by a newline
+# writeln will write this string to the serial UART device + a newline
 _writeln:
-	mv s0, ra # save return address since we do a `call` ourselves
+	push
 	call _write_uart # write the contents of a0 out
 	la a0, newline # point to our newline
 	call _write_uart # write out newline out
-	mv ra, s0 # set the return address back to what it was
+	pop
 	ret
 
 _setup_uart:
@@ -48,15 +96,19 @@ _setup_uart:
 	ret
 
 _write_uart:
+	push
+
+_write_uart_loop:
 	li t1, 0x10000000 # UART location
 	lb t2, 0(a0) # load the first char into t2
 	beqz t2, _write_uart_end # exit if we are at the string's end
 	sb t2, 0(t1) # write the char out to UART
 	li t2, 1 # 1 for incrementing
 	add a0, t2, a0 # increment to the next char position
-	j _write_uart # repeat until end of string 
+	j _write_uart_loop # repeat until end of string 
 
 _write_uart_end:
+	pop
 	ret
 
 _announce:
@@ -81,8 +133,6 @@ _announce:
 	la t2, _hartlock # point to our HART lock space
 	sw t1, 0(t2) # store the new HART ID into our lock that we stop on
 
-	fence # synchronizes I/O
-	
 	j _wait # done
 
 _wait:
